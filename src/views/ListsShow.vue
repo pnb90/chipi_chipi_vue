b6bbf10ae821767428156ee216a7e1eb611002da
pnb90 <phuocbui90@gmail.com>
2019-03-17 16:14:48 -0500

    made a table yay

--- src/views/ListsShow.vue
+++ src/views/ListsShow.vue
@@ -1,29 +1,72 @@
 <template>
   <div class="lists-show">
-    <h1>{{ list.name }} </h1>
-    <div v-for="(product, index) in list.products">
-      <!-- <h5> {{product}} </h5> -->
-      <h3>{{ product.name }}</h3>
-      <button class="btn-sm btn-danger" v-on:click="destroyListProduct(product.list_products)"> 
-      Remove Item 
+    <div class="container">
+      <h1 class="text-center">{{ list.name }} </h1>
+      <table class="table table-striped">
+        <thead class="thead-light">
+          <tr>
+            <th class="text-center" scope="col">Product</th>
+            <th class="text-center" scope="col">Quantity</th>
+            <th class="text-center" scope="col">Description</th>
+            <th class="text-center" scope="col">Store</th>
+            <th class="text-center" scope="col">Price</th>
+            <th class="text-center" scope="col">Got it!</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in list.products">
+            <td class="text-center"> {{ product.name }} </td>
+
+            <td class="text-center">
+              <div v-for="list_product in product.list_products">
+                <div v-if="list_product.list_id = $route.params.id">  
+                  {{ list_product.quantity }}
+                </div>
+              </div>
+            </td>
+
+            <td class="text-center">
+              <div v-for="list_product in product.list_products">
+                <div v-if="list_product.list_id = $route.params.id">  
+                  {{ list_product.description}}
+                </div>
+              </div>
+            </td>
+
+            <td>
+              <div class="store_name" v-for="inventory in product.inventories">
+                {{inventory.store.name}}  
+              </div>
+            </td>
+
+            <td>
+              <div class="price" v-for="inventory in product.inventories">
+                {{inventory.price}}
+              </div>
+            </td>
+
+            <td class="btn-sm btn-danger text-center" v-on:click="destroyListProduct(product.list_products)"> 
+                Remove Item 
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+
+      <button class="btn-sm btn-outline-success" type="button" data-toggle="collapse" data-target="#newItem">
+      Add Item 
       </button>
-    </div>
-    
-    <!-- What I need to get: the list product id via the product  -->
 
-    <button class="btn-sm btn-outline-success" type="button" data-toggle="collapse" data-target="#newItem">
-    Add Item 
-    </button>
-
-    <div class="collapse" id="newItem">
-      <h4>New Item</h4>
-      <form v-on:submit.prevent="submit()">
-        <h2>Add Product!</h2>
-        <p>Product ID #: <input v-model="newListProductProductId"></p>
-        <p>Quantity: <input v-model="newListProductQuantity"></p>
-        <p>Description: <input v-model="newListProductDescription"></p>
-        <input type="submit" value="Add Item">
-      </form>
+      <div class="collapse" id="newItem">
+        <form v-on:submit.prevent="submit()">
+          <h2>Add Product!</h2>
+          <!-- <p>Product Name: <input v-model="newListProductProductName"></p> -->
+          <p>Product ID #: <input v-model="newListProductProductId"></p>
+          <p>Quantity: <input v-model="newListProductQuantity"></p>
+          <p>Description: <input v-model="newListProductDescription"></p>
+          <input type="submit" value="Add Item">
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -54,6 +97,7 @@
                         ]
         },
         list_products: [],
+        // newListProductProductName: "",
         newListProductProductId: "",
         newListProductQuantity: "",
         newListProductDescription: "",
@@ -71,20 +115,22 @@
       destroyListProduct: function(list) {
         for (var i = 0; i < list.length; i++) {
           var hash = list[i]
-          if (hash.list_id === parseInt(this.$route.params.id)) {
+          if (hash.list_id === this.$route.params.id) {
             axios.delete("/api/list_products/" + hash.id)
             .then(response => {
+              console.log("Successfully deleted list_product", response.data);
               axios.get("/api/lists/" + this.$route.params.id )
-                          .then(response => {
-                            this.list = response.data;
-                          });
-            })
+              .then(response => {
+                this.list = response.data;
+              });
+            });
           }
         }
       },
       submit: function() {
         var params = {
                       list_id: this.list.id,
+                      // product_name: this.newListProductProductName,
                       product_id: this.newListProductProductId,
                       quantity: this.newListProductQuantity,
                       description: this.newListProductDescription
@@ -96,6 +142,9 @@
             axios.get("/api/lists/" + this.$route.params.id )
             .then(response => {
               this.list = response.data;
+              this.newListProductProductId = "";
+              this.newListProductQuantity = "";
+              this.newListProductDescription = "";
             });
           })
           .catch(error => {
